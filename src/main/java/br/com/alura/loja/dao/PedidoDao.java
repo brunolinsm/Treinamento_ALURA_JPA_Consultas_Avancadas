a24f01f7da4af787bc9bb35fb3ab998224b9339b
Bruno Lins Maia <bruno.maia@serpro.gov.br>
2025-05-05 15:44:12 -0300

    Consultas_com_JOIN_FETCH

--- src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -37,4 +37,11 @@ public class PedidoDao {
 
 		return em.createQuery(jpql, RelatorioDeVendasVo.class).getResultList();
 	}
+
+	public Pedido buscarPedidoComCliente(Long id) {
+		String jpql = "SELECT p FROM Pedido p JOIN FETCH p.cliente c WHERE p.id = :id";
+		return em.createQuery(jpql, Pedido.class)
+				.setParameter("id", id)
+				.getSingleResult();
+	}
 }
